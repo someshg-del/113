3b179fcc6449abbb675621b336d85dfdff54c3a8
someshg-del <76991902+someshg-del@users.noreply.github.com>
2021-05-10 16:05:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+function preload()
+ {
+
+
+}
+
+
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(150, 150);
+    viideo = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    fill(255, 0, 0);
+    stroke(255, 0, 0);
+    circle(50, 50, 80);
+    circle(50, 430, 80);
+    circle(590, 50, 80);
+    circle(590, 430, 80);
+    fill(0, 128, 0);
+    stroke(0, 128, 0);
+    rect(90, 40, 460, 20);
+    rect(90, 420, 460, 20);
+    rect(40, 90, 20, 300);
+    rect(580, 90, 20, 300);
+
+}
+
+function take_snapshot() {
+    save("myphoto.png")
+  }
